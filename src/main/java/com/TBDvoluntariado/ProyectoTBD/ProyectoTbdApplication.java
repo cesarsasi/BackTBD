8468619317ba4f3be29ca90e1915ea2b0e0b24c8
Cesar Salazar Silva <cesar.salazar@usach.cl>
2020-10-26 17:40:43 -0300

    Subir proyecto sin configurar

---
+++ src/main/java/com/TBDvoluntariado/ProyectoTBD/ProyectoTbdApplication.java
@@ -0,0 +1,13 @@
+package com.TBDvoluntariado.ProyectoTBD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoTbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoTbdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TBDvoluntariado/ProyectoTBD/ProyectoTbdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TBDvoluntariado.ProyectoTBD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoTbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
