4a4be7186cb00cc89c0aed862df2226d7fbc5938
César <cesar.salazar@gmail.com>
2020-11-08 20:19:41 -0300

    Añadir sql2o junto con configuracion de db en properties

--- src/main/java/com/TBDvoluntariado/ProyectoTBD/ProyectoTbdApplication.java
+++ src/main/java/com/TBDvoluntariado/ProyectoTBD/ProyectoTbdApplication.java
@@ -2,6 +2,8 @@ package com.TBDvoluntariado.ProyectoTBD;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ProyectoTbdApplication {
@@ -9,5 +11,7 @@ public class ProyectoTbdApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoTbdApplication.class, args);
 	}
+
+
 
 }
